b06fcec350b0b6c749f365f6f72f56220b774a65
muskan <muskantuteja714@gmail.com>
2025-06-30 11:52:14 +0530

    11:08

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Mortgage Calculator/' : '/',
+  base: process.env.NODE_ENV === 'production' ?  '/-mortgage-calculator/' : '/',
   plugins: [react()],
 })
